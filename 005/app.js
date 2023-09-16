dda9350842c6f0c63136f6c03694cfa02e5957c3
mohammedhrima <mohammedxhrima@gmail.com>
2023-09-16 14:52:12 +0100

    sep 16

--- 005/app.js
+++ 005/app.js
@@ -1,6 +1,6 @@
 var tag = "abc";
 
-const mini = {
+const Mini = {
     createElement: (tag, props, ...children) => {
         const element = {
             tag,
@@ -10,27 +10,26 @@ const mini = {
         };
         return element;
     },
-};
-
-function render(element, parent) {
-    if (["string", "number"].includes(typeof element)) {
-        parent?.appendChild(document.createTextNode(element?.toString()));
-        return;
-    }
-    const DOM_element = document.createElement(element.tag);
-    Object.keys(element?.props)
-        .filter((key) => key != "children")
-        .forEach((property) => {
-            DOM_element[property] = element.props[property];
+    render: (element, parent) => {
+        if (["string", "number"].includes(typeof element)) {
+            parent?.appendChild(document.createTextNode(element?.toString()));
+            return;
+        }
+        const DOM_element = document.createElement(element.tag);
+        Object.keys(element?.props)
+            .filter((key) => key != "children")
+            .forEach((property) => {
+                DOM_element[property] = element.props[property];
+            });
+        element?.props?.children.forEach((child) => {
+            render(child, DOM_element);
         });
-    element?.props?.children.forEach((child) => {
-        render(child, DOM_element);
-    });
-    parent.appendChild(DOM_element);
-}
+        parent.appendChild(DOM_element);
+    },
+};
 
 var tag1 = () => {
-    return <h1 >hello</h1>;
+    return <h1>hello</h1>;
 };
 
-render(tag1(), document.getElementById("mini"));
+Mini.render(tag1(), document.getElementById("mini"));
