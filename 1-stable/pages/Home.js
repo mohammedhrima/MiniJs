3440e75ae9c0287ce27b3673004461beb9ae6341
mohammedhrima <mohammedxhrima@gmail.com>
2024-01-28 14:01:31 +0100

    to be seen after

--- 1-stable/pages/Home.js
+++ 1-stable/pages/Home.js
@@ -2,33 +2,33 @@ import routes from "../main";
 import Mini from "../mini/mini";
 
 const Home2 = () => {
-    const [getB, setB] = Mini.useState(0);
-    const handle = () => {
-        setB(getB + 1);
-    };
+  const [getB, setB] = Mini.useState(0);
+  const handle = () => {
+    setB(getB + 1);
+  };
 
-    return (
-        <div>
-            <h2>A: {getB}</h2>
-            <button onclick={handle}>clique me A</button>
-        </div>
-    );
+  return (
+    <div style={{ backgroundColor: "blue"}}>
+      <h2>A: {getB}</h2>
+      <button onclick={handle}>clique me A</button>
+    </div>
+  );
 };
 
 const Home = () => {
-    const [getA, setA] = Mini.useState(0);
+  const [getA, setA] = Mini.useState(0);
 
-    const handle = () => {
-        setA(getA + 1);
-    };
+  const handle = () => {
+    setA(getA + 1);
+  };
 
-    return (
-        <div>
-            <h2>A: {getA}</h2>
-            <button onclick={handle}>clique me A</button>
-            <Home2/>
-        </div>
-    );
+  return (
+    <div>
+      <h2>A: {getA}</h2>
+      <button onclick={handle}>clique me A</button>
+      <Home2 />
+    </div>
+  );
 };
 
 export default Home;
