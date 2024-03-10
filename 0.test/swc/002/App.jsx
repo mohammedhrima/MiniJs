7b60f587f71593a818eb23f040b30ac6e7407cb4
Mohammed Hrima <mhrima@c3r8p3.1337.ma>
2024-03-10 21:02:54 +0000

    update

--- 0.test/swc/002/App.jsx
+++
@@ -1,9 +0,0 @@
-// global dependencies
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <h1>My SWC App</h1>;
-};
-
-ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ 0.test/swc/002/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MyComponent() {
+  return <div>yay</div>
+}
+
+export function add(a, b) {
+  return a + b
+}
