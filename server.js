54f910e3b24081a5edcfb0e32c15112a518b0708
Mohammed Hrima <mhrima@c3r8p4.1337.ma>
2024-06-09 02:34:20 +0100

    use http library instead of expressjs

--- server.js
+++ server.js
@@ -1,71 +1,72 @@
-const express = require("express");
-const path = require("path");
+const http = require("http");
 const fs = require("fs");
 const WebSocket = require("ws");
-const app = express();
+const path = require("path");
+
+// Define the port
 const PORT = 5000;
 
-app.use("/static", express.static(path.resolve(__dirname, "static")));
+const serveStaticFile = (filePath, contentType, res) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
+};
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
-});
-
-var url = `http://localhost:${PORT}`;
-var start =
-  process.platform == "darwin"
-    ? "open"
-    : process.platform == "win32"
-    ? "start"
-    : "xdg-open";
-require("child_process").exec(start + " " + url);
-
-const server = app.listen(PORT, () => {
-  console.log("server running... on port: ", PORT);
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    const indexPath = path.resolve(__dirname, "index.html");
+    serveStaticFile(indexPath, "text/html", res);
+  } else if (req.method === "GET" && req.url.startsWith("/static/")) {
+    const filePath = path.resolve(__dirname, "." + req.url);
+    const ext = path.extname(filePath);
+    let contentType = "text/plain";
+    switch (ext) {
+      case ".js":
+        contentType = "application/javascript";
+        break;
+      case ".css":
+        contentType = "text/css";
+        break;
+      case ".html":
+        contentType = "text/html";
+        break;
+    }
+    serveStaticFile(filePath, contentType, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
 });
 
 const wss = new WebSocket.Server({ server });
 
-// Watch for changes in /static/index.js
-const indexPath = path.resolve(__dirname, "static", "index.js");
-fs.watch(indexPath, (eventType, filename) => {
-  if (eventType === "change") {
-    // console.log("/static/index.js has been modified.");
-    // Notify all connected clients about the change
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send("refresh");
-      }
-    });
-  }
-});
+function listenOnChange(filePath) {
+  fs.watch(filePath, (eventType, filename) => {
+    if (eventType === "change") {
+      console.log(filePath, "has been modified.");
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send("refresh");
+        }
+      });
+    }
+  });
+}
 
-const htmlPath = path.resolve(__dirname, "index.html");
-fs.watch(htmlPath, (eventType, filename) => {
-  if (eventType === "change") {
-    console.log("index.html has been modified.");
-    // Notify all connected clients about the change
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send("refresh");
-      }
-    });
-  }
-});
-
-const cssPath = path.resolve(__dirname, "static", "index.css");
-fs.watch(cssPath, (eventType, filename) => {
-  if (eventType === "change") {
-    console.log("index.css has been modified.");
-    // Notify all connected clients about the change
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send("refresh");
-      }
-    });
-  }
-});
+listenOnChange(path.resolve(__dirname, "static", "index.js"));
+listenOnChange(path.resolve(__dirname, "static", "index.css"));
+listenOnChange(path.resolve(__dirname, "index.html"));
 
 wss.on("connection", (ws) => {
   console.log("Client connected");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
